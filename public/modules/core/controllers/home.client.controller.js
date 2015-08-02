720f54680becbd50d0dc4a7e11845a5ad6b05194
dbajramovi1@etf.unsa.ba <dbajramovi1@etf.unsa.ba>
2015-08-02 23:41:05 +0200

    css dorada

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,6 +5,18 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication, Articles) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function() {
+            slides.push({
+                image: 'http://res.cloudinary.com/kengurjs/image/upload/v1438546488/11823801_1105702629444113_1079615523_n_tkn2qy.jpg',
+                text: 'KK Kengur'
+            });
+        };
+        for (var i=0; i<4; i++) {
+            $scope.addSlide();
+        }
         $scope.broj_clanaka = 1;
         $scope.shownarticles = [];
         $scope.test = 'OVDJE';
